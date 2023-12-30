36a5c92373a85d0b3c21d2e6195d5ad279816ddb
YeganaKhalilova <116161213+YeganaKhalilova@users.noreply.github.com>
2023-12-30 20:30:31 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Bankfresh-Bank-Network-web-app-MERN-STACK
+React , Express, Node JS, Mongo db etc
---
+++ server/routes/auth.js
@@ -0,0 +1,25 @@
+const express=require('express');
+const router=express.Router();
+const cors = require('cors'); 
+const{registerUser,loginUser,getUserData}=require('../controllers/auth');
+const authenticateToken = require('../middlewares/auth');
+
+
+//middleware
+
+router.use(
+    cors(
+        {
+            credentials:true,
+            origin:'http://localhost:3000'
+        }
+    )
+)
+
+
+router.post('/signup', registerUser)
+router.post('/login', loginUser)
+
+router.get('/user-data', authenticateToken, getUserData);
+
+module.exports=router;
